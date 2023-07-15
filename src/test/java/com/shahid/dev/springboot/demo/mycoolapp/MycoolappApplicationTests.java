b108629c4165d588e022b7cfcd6715bb96ddf4ab
shahid sarwar <shahidsarwar@shahids-MacBook-Pro.local>
2023-07-14 22:04:04 -0700

    [1] First Master commit

---
+++ src/test/java/com/shahid/dev/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahid.dev.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
